1a6e8ea090aeb21e662d432cce8f51dd6f2a5637
Carlos Vieira Charlie <101368855+charlieveira@users.noreply.github.com>
2023-05-29 12:12:38 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,13 @@
+var password = document.getElementById("pass")
+  , confirm_password = document.getElementById("passC");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Senhas diferentes!");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
